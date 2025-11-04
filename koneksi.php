d1b5343acfc43dbd6af8bd7dc394b8b14c5e6f1d
wayanwisnu1 <jacobwisnu@gmail.com>
2025-11-04 14:12:31 +0700

    Menambahkan file koneksi database

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "db_tugas";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+} else {
+    echo "Koneksi berhasil!";
+}
